2b0cbe718e649226eb28e768ec51e392c07a4e6c
QianxiW <113392048+QianxiW@users.noreply.github.com>
2022-10-31 03:11:02 -0400

    Update BagMain.java

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -16,6 +16,12 @@ class BagMain {
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
+       for (Bag x : bags){
+            x.enhance();
+            if (double_enhance_handbags && x instanceof HandBag){
+                x.enhance();
+            }
+        }
     }
 
     /**
@@ -29,5 +35,12 @@ class BagMain {
      */
     public static int countCrossbodyStraps(Bag[] bags) {
         // TODO: Implement this.
+        int totalStraps = 0;
+        for (Bag x : bags){
+            if (x instanceof CrossbodyBag){
+                totalStraps += ((CrossbodyBag) x).getNumberOfStraps();
+            }
+        }
+        return totalStraps;
     }
 }
