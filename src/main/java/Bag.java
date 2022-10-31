2ed6a80bf847f1d0427947d3082ac4ef523535b8
QianxiW <113392048+QianxiW@users.noreply.github.com>
2022-10-31 03:09:51 -0400

    Update Bag.java

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -13,6 +13,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
 
@@ -26,6 +30,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
+    public Bag (String color, int capacity){
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[0];
+    }
 
 
 
@@ -37,6 +47,16 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+     public String getColor(){
+        return this.color;
+    }
+
+    public int getNumberOfContents() {
+        return numberOfContents;
+    }
+    public int getCapacity(){
+        return capacity;
+    }
 
 
 
@@ -45,6 +65,9 @@ public abstract class Bag {
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
+    public String setColor(String color){
+        return this.color = color;
+    }
 
 
 
@@ -60,6 +83,18 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
+    public Boolean addItem(String item){
+        if(item.length() < this.capacity){
+            String[] New = new String[contents.length+1];
+            System.arraycopy(contents, 0, New, 0, contents.length);
+            New[contents.length] = item;
+            contents = New;
+            numberOfContents += item.length();
+            return true;
+        }
+        return false;
+    }
+
 
 
 
@@ -75,6 +110,22 @@ public abstract class Bag {
      *
      * @return
      */
+    public String popItem(){
+
+        if (contents.length == 0){
+            return null;
+        }
+
+        String[] New = new String[contents.length - 1];
+        String lastItem = contents[contents.length - 1];
+
+        System.arraycopy(contents, 0, New, 0, contents.length - 1);
+
+        contents = New;
+        numberOfContents = contents.length;
+
+        return lastItem;
+    }
 
 
 
